0e0c9cae3ac25e54e3e96091f9b442af28f3d034
Catherine Broker <catherine@CatherinenoMacBook-puro.local>
2018-12-14 15:52:45 +0900

    media queries

--- catherinemariebroker/js/script.js
+++ catherinemariebroker/js/script.js
@@ -14,3 +14,18 @@ function myFunction() {
 };
 
 /* Fade Out on Scroll Effect */
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var tags = $("section")
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible")
+    } else {
+      $(tag).removeClass("visible")
+    }
+  }
+})
