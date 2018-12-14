f30286ab8c3f41779f932d20153778b489654124
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2018-12-14 14:30:41 +0900

    Add files via upload

---
+++ catherinemariebroker/js/script.js
@@ -0,0 +1,16 @@
+/* Hamburger menu change to "x" Function */
+function hamburgerX(x) {
+    x.classList.toggle("change");
+}
+
+/* NavBar Responsive Function*/
+function myFunction() {
+    var x = document.getElementById("NavBar");
+    if (x.className === "NavBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "NavBar";
+    }
+};
+
+/* Fade Out on Scroll Effect */
