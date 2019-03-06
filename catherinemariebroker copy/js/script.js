2a8bf6e055dd0ef0a35ec745906174759675b988
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2019-03-06 15:14:02 +0900

    Updates

--- catherinemariebroker copy/js/script.js
+++ catherinemariebroker copy/js/script.js
@@ -1,14 +1,45 @@
-/* Hamburger menu change to "x" Function */
-function hamburgerX(x) {
-    x.classList.toggle("change");
-}
+$(function(){
+  //Smooth Scroll
+  $("a").on('click', function(event) {
 
-/* NavBar Responsive Function*/
-function myFunction() {
-    var x = document.getElementById("NavBar");
-    if (x.className === "NavBar") {
-        x.className += " responsive";
-    } else {
-        x.className = "NavBar";
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
     }
-};
+  });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* Hamburger menu change to "x" Function */
+// function hamburgerX(x) {
+//     x.classList.toggle("change");
+// }
+//
+// /* NavBar Responsive Function*/
+// function myFunction() {
+//     var x = document.getElementById("NavBar");
+//     if (x.className === "NavBar") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "NavBar";
+//     }
+// };
