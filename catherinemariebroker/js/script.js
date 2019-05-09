2993c0d300d63681b689d1635cfb3e711daeca9a
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2019-05-09 12:24:54 +0900

    Site OVERHAUL
    Basically finished the structure. Just need to translate the Japanese pages and add more 作品.

--- catherinemariebroker/js/script.js
+++ catherinemariebroker/js/script.js
@@ -1,14 +1,105 @@
-/* Hamburger menu change to "x" Function */
-function hamburgerX(x) {
-    x.classList.toggle("change");
-}
+$(document).ready(function() {
 
-/* NavBar Responsive Function*/
-function myFunction() {
-    var x = document.getElementById("NavBar");
-    if (x.className === "NavBar") {
-        x.className += " responsive";
+/************ ALL PAGES ***************/
+
+  /* Add smooth scrolling to all links*/
+  $("a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+      });
+    };
+  });
+
+
+  /* Sparkling Effect */
+    var body = $('#starshine'),
+    template = $('.template.shine'),
+    stars =  100,
+    sparkle = 80;
+
+    var size = 'small';
+    var createStar = function() {
+      template.clone().removeAttr('id').css({
+       top: (Math.random() * 100) + '%',
+       left: (Math.random() * 100) + '%',
+       webkitAnimationDelay: (Math.random() * sparkle) + 's',
+       mozAnimationDelay: (Math.random() * sparkle) + 's'
+     }).addClass(size).appendTo(body);
+   };
+
+   for(var i = 0; i < stars; i++) {
+     if(i % 2 === 0) {
+       size = 'small';
+     } else if(i % 3 === 0) {
+       size = 'medium';
+     } else {
+       size = 'large';
+     }
+     createStar();
+   }
+
+
+/************ INDEX PAGE ***************/
+  /* Content Fade on Scroll */
+  // $(window).scroll(function(){
+  //     $("#section_top").css("opacity", 1 - $(window).scrollTop() / 350);
+  //   });
+  //
+  // $(window).scroll(function(){
+  //     $("#section_services").css("opacity", 1.5 - $(window).scrollTop() / 1000);
+  //   });
+  //
+  // $(window).scroll(function(){
+  //     $("#section_work").css("opacity", 1.5 - $(window).scrollTop() / 2500);
+  //   });
+
+
+
+/************ ABOUT PAGE ***************/
+//Animate Skills Bars after scrolling */
+$(window).scroll(function(){
+    $("#html5").addClass("html5");
+    $("#css3").addClass("css3");
+    $("#javascript").addClass("javascript");
+    $("#jquery").addClass("jquery");
+    $("#php").addClass("php");
+    $("#mysql").addClass("mysql");
+    $("#wordpress").addClass("wordpress");
+    $("#bootstrap").addClass("bootstrap");
+    $("#adobecloud").addClass("adobecloud");
+  });
+
+
+
+}); /*End JQuery*/
+
+  /* Responsive Mobile Nav Script*/
+  function navFunction() {
+    var x = document.getElementById("navBar_mobile_links");
+    if (x.className === "navBar_mobile_links") {
+      x.className += "responsive";
     } else {
-        x.className = "NavBar";
+      x.className = "navBar_mobile_links";
     }
-};
+    var y = document.getElementById("navBar_mobile");
+    if(y.className === "navBar_mobile") {
+      y.className += "navMargin";
+    } else {
+      y.className = "navBar_mobile";
+    }
+  }
